52501ba9e792427c2a15a446799f84671c7c44a9
Göksel Özardalı <gokselozardali@gmail.com>
2020-07-27 11:39:28 +0300

    takımlar oluşturuldu.

---
+++ database/seeds/TeamsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TeamsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    const TEAM_COUNT = 4;
+
+    public function run()
+    {
+        DB::table('teams')->delete();
+        for ($i = 0; $i <= self::TEAM_COUNT; $i++) {
+            $now = \Carbon\Carbon::now();
+            DB::table('teams')->insert([
+                'name' => Str::random(6). ' FC',
+                'strength' => rand(30, 100),
+                'created_at' => $now,
+                'updated_at' => $now
+            ]);
+
+        }
+    }
+}
